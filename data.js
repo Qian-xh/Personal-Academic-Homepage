0fc88fa986b82d31ac0a3883417ba49c58bc81d9
Qian Xiaohe <qxh2491@163.com>
2026-06-06 09:21:31 +0800

    Update publication list

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 window.siteData = {
-  lastUpdated: "2026-05-29",
+  lastUpdated: "2026-06-06",
   profile: {
     displayName: "Qian Xiaohe",
-    chineseName: "钱晓鹤",
+    chineseName: "\u94b1\u6653\u9e64",
     initials: "QX",
     gender: "Male",
     institution: "Institute of Geographic Science and Natural Resources Research, CAS",
@@ -39,31 +39,94 @@ window.siteData = {
   publications: [
     {
       year: "2026",
-      title: "Spatiotemporal evolution characteristics and driving forces of flash-flood hazards in the upper Minjiang River",
-      authors: "Chen Xinsen, Zhang Guotao, Qian Xiaohe, Zhang Junlong, et al.",
-      venue: "ResearchGate indexed record",
-      link: "https://www.researchgate.net/publication/402030037_minjiangshangyoushanhongzaihaideshikongyanbiantezhengjiqudongli"
+      title: "Mosses and cyanobacteria increased soil moisture and alleviated soil dehydration caused by dry-wet alternation in subtropical humid region",
+      authors: "Ruyi Zi, Zhen Han, Qian Fang, Xiaoai Yin, Fayong Fang, Xiaohe Qian, Sheng Li, Longshan Zhao",
+      venue: "CATENA, 264, 109792. DOI: 10.1016/j.catena.2026.109792",
+      link: "https://doi.org/10.1016/j.catena.2026.109792"
+    },
+    {
+      year: "2026",
+      title: "Spatiotemporal evolution characteristics and driving forces of flash flood disasters in upper Minjiang River basin",
+      authors: "Chen Xinsen, Zhang Guotao, Qian Xiaohe, et al.",
+      venue: "Journal of Soil and Water Conservation, 40(1), 297-309. DOI: 10.13870/j.cnki.stbcxb.2026.01.030",
+      link: "https://www.stbcxb.com.cn/en/article/doi/10.13870/j.cnki.stbcxb.2026.01.030/"
+    },
+    {
+      year: "2026",
+      title: "Research progress on observation index system and technology for water-soil processes of flash floods and debris flows",
+      authors: "Zhang Guotao, Cui Peng, Guo Jian, Li Chaoyue, Zhang Xingdi, Qian Xiaohe, Song Zhiri, Si Wen",
+      venue: "China Flood & Drought Management. Online first: 14 May 2026",
+      link: ""
     },
     {
       year: "2024",
       title: "Effect of bedrock permeability on runoff and soil loss in soil-mantled karst slopes under successive rainfall conditions",
-      authors: "Qian Fang, Ruyi Zi, Longshan Zhao, et al., Xiaohe Qian",
-      venue: "CATENA",
-      link: "https://www.researchgate.net/scientific-contributions/Chunhua-Fan-2187143088"
+      authors: "Qian Fang, Ruyi Zi, Longshan Zhao, Chunhua Fan, et al.",
+      venue: "CATENA, 247, 108524. DOI: 10.1016/j.catena.2024.108524",
+      link: "https://doi.org/10.1016/j.catena.2024.108524"
     },
     {
-      year: "2023",
+      year: "2024",
+      title: "Quantifying the effects of rock fragments embedding vs. covering on soil erosion in karst sloping cropland",
+      authors: "Ruyi Zi, Zhen Han, Tingsheng Chen, Fayong Fang, et al.",
+      venue: "CATENA, 244, 108234. DOI: 10.1016/j.catena.2024.108234",
+      link: "https://doi.org/10.1016/j.catena.2024.108234"
+    },
+    {
+      year: "2024",
+      title: "Effect of Nostoc commune cover on shallow soil moisture, runoff and erosion in the subtropics",
+      authors: "Ruyi Zi, Longshan Zhao, Qian Fang, Fayong Fang, Xiaoai Yin, Xiaohe Qian, Chunhua Fan, Zhen Han",
+      venue: "Geoderma, 447, 116931. DOI: 10.1016/j.geoderma.2024.116931",
+      link: "https://doi.org/10.1016/j.geoderma.2024.116931"
+    },
+    {
+      year: "2024",
+      title: "Quantification of rill development and erosion characteristics on hillslopes driven by upslope inflow",
+      authors: "Qian Xiaohe",
+      venue: "Master's thesis, Guizhou University",
+      link: ""
+    },
+    {
+      year: "2024",
       title: "Rill formation and evolution caused by upslope inflow and sediment deposition on freshly tilled loose surfaces",
-      authors: "Xiaohe Qian, Longshan Zhao, Qian Fang, et al.",
-      venue: "Soil and Tillage Research",
+      authors: "Xiaohe Qian, Longshan Zhao, Qian Fang, Chunhua Fan, Ruyi Zi, Fayong Fang",
+      venue: "Soil and Tillage Research, 235, 105868. DOI: 10.1016/j.still.2023.105868",
       link: "assets/qian-xiaohe-rill-formation-2024.pdf"
     },
     {
+      year: "2023",
+      title: "Path analysis of the effects of hydraulic conditions, soil properties and plant roots on the soil detachment capacity of karst hillslopes",
+      authors: "Ruyi Zi, Longshan Zhao, Qian Fang, Xiaohe Qian, Fayong Fang, Chunhua Fan",
+      venue: "CATENA, 228, 107177. DOI: 10.1016/j.catena.2023.107177",
+      link: "https://doi.org/10.1016/j.catena.2023.107177"
+    },
+    {
+      year: "2023",
+      title: "How do belowground bedrock fissures impact soil erosion on karst hillslopes under different rainfall intensity conditions?",
+      authors: "Qian Fang, Longshan Zhao, Chunhua Fan, Fayong Fang, et al.",
+      venue: "Land Degradation & Development, 34(6), 1830-1841. DOI: 10.1002/ldr.4572",
+      link: "https://doi.org/10.1002/ldr.4572"
+    },
+    {
       year: "2022",
-      title: "Influencing factors of rainwater transformation and soil erosion in thin-soil hillslopes of rocky desertification regions",
+      title: "Relationship between soil anti-scourability and soil physical properties in karst mountain areas",
+      authors: "Ruyi Zi, Longshan Zhao, Xiaohe Qian, Juan Chen",
+      venue: "Journal of Soil and Water Conservation, 36(3), 23-29. DOI: 10.13870/j.cnki.stbcxb.2022.03.004",
+      link: "http://www.stbcxb.com.cn/thesisDetails#10.13870/j.cnki.stbcxb.2022.03.004"
+    },
+    {
+      year: "2022",
+      title: "Influencing factors of rainwater transformation and soil erosion in thin soil hillslope of rock desertification regions",
       authors: "Fang Qian, Longshan Zhao, Chunhua Fan, Kaifeng Li, Fayong Fang, Xiaohe Qian",
-      venue: "Transactions of the Chinese Society of Agricultural Engineering",
+      venue: "Transactions of the Chinese Society of Agricultural Engineering, 38(8), 88-96. DOI: 10.11975/j.issn.1002-6819.2022.08.011",
       link: "https://www.aeeisp.com/nygcxb/en/article/Y2022/I8/88"
+    },
+    {
+      year: "2022",
+      title: "Soil erosion characteristics of different soil and rock mosaic slopes under the condition of successive rainfalls",
+      authors: "Chunhua Fan, Longshan Zhao, Qian Fang, Kaifeng Li, Xiaohe Qian",
+      venue: "Acta Pedologica Sinica, 59(5), 1270-1278. DOI: 10.11766/trxb202011020606",
+      link: "https://pedologica.issas.ac.cn/html/trxb/2022/5/trxb202011020606.htm"
     }
   ],
   contacts: [
